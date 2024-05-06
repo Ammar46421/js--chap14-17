eecd3f373ca65e6dfadb039c788e4a809fd3b754
Ammar46421 <153704851+Ammar46421@users.noreply.github.com>
2024-05-06 09:46:42 +0500

    practice

---
+++ link.js
@@ -0,0 +1,47 @@
+// chapter 14-15
+var city1 = "karachi";
+var city2 = "lahore";
+var city3 = "islamabad";
+var city4 = "hyderabad";
+var city5 = "sukkur";
+
+var city = prompt("Enter Your City");
+
+if (city === city1) {
+    alert("Mobile Nikal ------")
+};
+if (city === city2) {
+    alert("Lahore Lahore ee")
+};
+if (city === city3) {
+    alert("hi bro whassup")
+};
+if (city === city4) {
+    alert("ada chanh piyanden?")
+};
+if (city === city5) {
+    alert("dawat athai location kachaa")
+};
+// var city1 = ["Karachi", "karachi"]
+// var city2 = ["Lahore", "lahore"]
+// var city3 = ["Islamabad", "islamabad"]
+// var city4 = ["hyderabad", "Hyderabad"]
+// var city5 = ["Sukkur", "sukkur"]
+
+// var city = prompt("Enter Your City")
+// if (city === city1) {
+//     alert("Mobile Nikal ------")
+// };
+// if (city === city2) {
+//     alert("Lahore Lahore ee")
+// };
+
+// if (city === city3) {
+//     alert("hi bro whassup")
+// };
+// if (city === city4) {
+//     alert("ada chanh piyanden?")
+// };
+// if (city === city5) {
+//     alert("dawat athai location kachaa")
+// };
